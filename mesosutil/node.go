ebd20ea0db688bc0a2aa0cfaeb223167461f250d
Gilbert Song <songzihao1990@gmail.com>
2015-10-12 14:04:01 -0700

    fix hostname bug using 'uname -n'

--- mesosutil/node.go
+++ mesosutil/node.go
@@ -9,15 +9,18 @@ import (
 
 //TODO(jdef) copied from kubernetes/pkg/util/node.go
 func GetHostname(hostnameOverride string) string {
-	hostname := []byte(hostnameOverride)
+	hostname := hostnameOverride
 	if string(hostname) == "" {
 		// Note: We use exec here instead of os.Hostname() because we
 		// want the FQDN, and this is the easiest way to get it.
-		fqdn, err := exec.Command("hostname", "-f").Output()
+		nodename, err := exec.Command("uname", "-n").Output()
 		if err != nil {
 			log.Fatalf("Couldn't determine hostname: %v", err)
 		}
-		hostname = fqdn
+		chunks := strings.Split(string(nodename), ".")
+		// nodename could be a fully-qualified domain name or not. Take the
+		// first word of nodename as the hostname for consistency.
+		hostname = chunks[0]
 	}
 	return strings.TrimSpace(string(hostname))
 }
